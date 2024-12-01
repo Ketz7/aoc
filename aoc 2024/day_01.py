bcf3e19fe36c336d948f0c893b83e31b55f48893
unknown <ketanmaurya@gmail.com>
2024-12-01 15:28:45 +0530

    Day 1 Solution"

--- aoc 2024/day_01.py
+++ aoc 2024/day_01.py
@@ -1,18 +1,31 @@
-import aoc_lube
+import json
 
-RAW = aoc_lube.fetch(year=2024, day=1)
-print(RAW)
+with open('input_day01.txt') as f:
+    content = f.read()  
+    lines = content.splitlines()  
 
-def parse_raw():
-    ...
 
-DATA = parse_raw()
+# Day 1 P 1
 
-def part_one():
-    ...
+a, b = [], []
+for line in lines:
+    num1, num2 = map(int, line.split())
+    a.append(num1)
+    b.append(num2)
 
-def part_two():
-    ...
+a_sort = sorted(a)
+b_sort = sorted(b)
 
-aoc_lube.submit(year=2024, day=1, part=1, solution=part_one)
-aoc_lube.submit(year=2024, day=1, part=2, solution=part_two)
+subtraction = [abs(x - y) for x,y in zip(a_sort,b_sort)]
+
+print("Day 1, Part 1 Answer: ", sum(subtraction))
+
+# Day 1 P 2
+
+hashmap = {x: (b.count(x), a.count(x)) for x in a if b.count(x) > 0}
+
+result = {key: key * (value[0] * value[1]) for key,value in hashmap.items()}
+
+total_sum = sum(result.values())
+
+print("Day 1, Part 2 Answer: ", total_sum)
