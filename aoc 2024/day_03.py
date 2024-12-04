d3bf2c0250831d3cbf7cc9b59a8034cd81d8644f
unknown <ketanmaurya@gmail.com>
2024-12-04 18:53:58 +0530

    Day 4 Solution

--- aoc 2024/day_03.py
+++ aoc 2024/day_03.py
@@ -1,9 +1,13 @@
+from aoc_helper import *
+
 import re
 
 with open('input_3.txt') as f:
     content = f.read().strip()
 
 # P 1
+x = re.findall("mul\(\d+,\d+\)", content)
+
 data = re.findall(r'mul\((\d+,\d+)\)', content)
 
 result = sum(int(x) * int(y) for pair in data for x,y in [pair.split(',')])
@@ -11,6 +15,21 @@ result = sum(int(x) * int(y) for pair in data for x,y in [pair.split(',')])
 print('Part 1: ', result)
 
 # P 2
+x = re.findall("mul\(\d+,\d+\)|do\(\)|don't\(\)", content)
+
+ans = 0
+g = True
+
+for i in x:
+    if i == 'do()':
+        g = True
+    elif i == "don't()":
+        g = False
+    else:
+        if g:
+            a, b = nums(i)
+            ans += a * b
+print(ans)
 
 content = re.sub(r"don't\(\).*?(?:$|do\(\))", '', content, flags=re.DOTALL)
 
--- aoc 2024/day_04.py
+++ aoc 2024/day_04.py
@@ -1,18 +1,30 @@
-import aoc_lube
+grid = open("input_4.txt").read().splitlines()
 
-RAW = aoc_lube.fetch(year=2024, day=4)
-print(RAW)
+# P 1
 
-def parse_raw():
-    ...
+p1_count = 0
 
-DATA = parse_raw()
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        if grid[r][c] != "X": continue
+        for dr in [-1,0,1]:
+            for dc in [-1,0,1]:
+                if dr == dc == 0: continue
+                if not (0 <= r + 3 * dr < len(grid) and 0 <= c + 3 * dc < len(grid[0])): continue
+                if grid[r + dr][c + dc] == "M" and grid[r + 2 * dr][c + 2 * dc] == "A" and grid[r + 3 *dr][c + 3 * dc] == "S":
+                    p1_count += 1
 
-def part_one():
-    ...
+print(p1_count)
 
-def part_two():
-    ...
+# P 2
+ 
+p2_count = 0
 
-aoc_lube.submit(year=2024, day=4, part=1, solution=part_one)
-aoc_lube.submit(year=2024, day=4, part=2, solution=part_two)
+for r in range(1, len(grid) - 1):
+    for c in range(1, len(grid[0]) - 1):
+        if grid[r][c] != "A": continue
+        corners = [grid[r -1 ][c - 1], grid[r - 1][c + 1], grid[r + 1][c + 1], grid[r+ 1][c -1] ]
+        if "".join(corners) in ["MMSS", "MSSM", "SSMM", "SMMS"]:
+            p2_count += 1
+
+print(p2_count)
