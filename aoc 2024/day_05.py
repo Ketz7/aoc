49cde9afa52c38e1b86bc96f7fd9ba3fbb44e941
unknown <ketanmaurya@gmail.com>
2024-12-05 15:38:16 +0530

    Day 5 Solution

--- aoc 2024/day_05.py
+++ aoc 2024/day_05.py
@@ -1,18 +1,55 @@
-import aoc_lube
+import functools
 
-RAW = aoc_lube.fetch(year=2024, day=5)
-print(RAW)
+file = open(r"input_5.txt")
 
-def parse_raw():
-    ...
+rules = []
 
-DATA = parse_raw()
+for line in file:
+    if line.isspace(): break
+    rules.append(list(map(int, line.split("|"))))
 
-def part_one():
-    ...
+cache = {}
 
-def part_two():
-    ...
+for x,y in rules:
+    cache[(x,y)] = True
+    cache[(y,x)] = False
 
-aoc_lube.submit(year=2024, day=5, part=1, solution=part_one)
-aoc_lube.submit(year=2024, day=5, part=2, solution=part_two)
+cache_2 =  {}
+
+for x,y in rules:
+    cache_2[(x,y)] = -1
+    cache_2[(y,x)] = 1
+
+def is_ordered_2(update):
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            key = (update[i], update[j])
+            if key in cache_2 and cache_2[key] == 1:
+                return False
+    return True
+
+def cmp(x,y):
+    return cache_2.get((x,y), 0)
+
+def is_ordered(update):
+    for i in range(len(update)):
+        for j in range(i + 1, len(update)):
+            key = (update[i], update[j])
+            if key in cache and not cache[key]:
+                return False
+    return True
+
+total = 0
+
+total_2 = 0 
+
+for line in file:
+    update = list(map(int, line.split(",")))
+    if is_ordered(update):
+        total += update[len(update) // 2]
+    if is_ordered_2(update): continue
+    update.sort(key=functools.cmp_to_key(cmp))
+    total_2 += update[len(update) // 2]
+
+print(total)
+print(total_2)
